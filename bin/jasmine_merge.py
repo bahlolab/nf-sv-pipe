e81ed62dae655ebd3ecd9fccf5dd70f99fdeb441
jemunro <jacob.e.munro@gmail.com>
2021-11-26 20:49:54 +1100

    progress on jasmine wrapper script

--- bin/jasmine_merge.py
+++ bin/jasmine_merge.py
@@ -1,39 +1,44 @@
 #!/usr/bin/env python3
 import os.path
-
 from pysam import VariantFile
 import re
 import argparse
 import subprocess
 from subprocess import Popen
+import time
 
 MODE = {'bcf': 'b', 'vcf': 'v', 'gz': 'z'}
 REMOVE = ['^INFO/SVTYPE', '^INFO/SVLEN', '^INFO/END', '^INFO/STRANDS',
           '^INFO/PRECISE', '^INFO/IMPRECISE', 'FORMAT']
+WORK = 'work'
 
 
 class VcfWriter:
-    def __init__(self, output, header, remove=None):
+    def __init__(self, output, header, drop=False, remove=None, sort=False):
         mode = MODE[file_ext(output)]
         self.output = output
+        cmd = []
         if remove:
-            cmd = ['bcftools', 'annotate', '--no-version', '--remove', ','.join(remove),
-                   '-O{}'.format(mode), '-o', output]
-        else:
-            cmd = ['bcftools', 'view', '--no-version', '-O{}'.format(mode), '-o', output]
-        self.proc = Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            cmd.extend(['bcftools', 'annotate', '--no-version', '-Ou', '--remove', ','.join(remove), '|'])
+        if sort:
+            cmd.extend(['bcftools', 'sort', '-Ou', '-T', WORK, '|'])
+        cmd.extend(['bcftools', 'view', '--no-version', '-O{}'.format(mode), '-o', output])
+        if drop:
+            cmd.extend(['-G'])
+        self.proc = Popen(' '.join(cmd), stdin=subprocess.PIPE,stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL,
+                          shell=True)
         self.variantFile = VariantFile(self.proc.stdin, 'wu', header=header)
 
     def index(self):
-        proc = Popen(['bcftools', 'index', self.output],
+        proc = Popen(['bcftools', 'index', '-f', self.output],
                       stdin=subprocess.PIPE, stdout=subprocess.DEVNULL)
+        proc.wait()
         return proc.poll()
 
     def close(self, index=False):
         self.variantFile.close()
         self.proc.communicate()
         if index:
-            self.proc.poll()
             return self.index()
         return self.proc.poll()
 
@@ -53,58 +58,151 @@ def get_sv_type(record):
     return 'NONE'
 
 
-def strip_vcfs(inputs, work, split_types):
+def proc_vcfs(inputs, sv_types):
     vcfs = {}
+    records = {}
     for input in inputs:
-        pref = '{}/{}'.format(work, re.sub('(.vcf(.gz)?)|(.bcf)$', '', input))
+        pref = re.sub('(.vcf(.gz)?)|(.bcf)$', '', input)
         vf = VariantFile(input)
+        # samples.extend(list(vf.header.samples))
         outs = {}
-        for rec in vf:
-            sv_type = get_sv_type(rec) if split_types else 'ALL'
+        records[pref] = {'HEADER': vf.header}
+        for i, rec in enumerate(vf):
+            sv_type = get_sv_type(rec)
+            if sv_types and sv_type not in sv_types:
+                continue
+            rec.id = pref + '_' + str(i)
             if sv_type not in outs:
-                outs[sv_type] = VcfWriter("{}.{}.vcf".format(pref, sv_type), vf.header, remove=REMOVE)
+                outs[sv_type] = VcfWriter("{}/{}.{}.vcf".format(WORK, pref, sv_type), vf.header, remove=REMOVE)
+                records[pref][sv_type] = {}
             outs[sv_type].write(rec)
+            records[pref][sv_type][rec.id] = rec
         for sv_type, vw in outs.items():
             if sv_type not in vcfs:
                 vcfs[sv_type] = [vw.output]
             else:
                 vcfs[sv_type].append(vw.output)
             vw.close()
+    return vcfs, records
+
+
+def get_jasmine_cp(jasmine_dir):
+    jasmine_jar = jasmine_dir + '/jasmine.jar'
+    iris_jar = jasmine_dir + '/jasmine_iris.jar'
+    if not os.path.isfile(jasmine_jar) or not os.path.isfile(iris_jar):
+        print("jasmine.jar/jasmine_iris.jar not found in {}".format(jasmine_dir))
+        exit(1)
+    return iris_jar + ':' + jasmine_jar
+
+
+def jasmine(input_vcfs, jasmine_cp, args):
+    vcfs = {}
+    for sv_type in input_vcfs:
+        input_list = '{}/{}.txt'.format(WORK, sv_type)
+        output = '{}/{}.vcf'.format(WORK, sv_type)
+        with open(input_list, "w") as out:
+            out.write("\n".join(input_vcfs[sv_type]))
+        cmd = ['java', '-cp', jasmine_cp, 'Main',
+               'iris_args=samtools_path=samtools,racon_path=racon,minimap_path=minimap2',
+               '--keep_var_ids',
+               'file_list={}'.format(input_list),
+               'out_file={}'.format(output),
+               'out_dir={}'.format(WORK)]
+        cmd.extend(args)
+        print(' '.join(cmd))
+        proc = Popen(cmd, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        proc.wait()
+        if proc.poll() != 0:
+            print("jasmine error")
+            exit(1)
+        proc = Popen(['bcftools', 'sort', output, '-Oz', '-o', '{}.gz'.format(output)],
+                     stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        proc.wait()
+        vcfs[sv_type] = '{}.gz'.format(output)
     return vcfs
 
 
-def jasmine(input_vcfs, out_vcf, jasmine_dir, work, threads=2, max_dist_linear=0.2, min_overlap=0.8):
-    for sv_type in input_vcfs:
-        input_list = '{}/{}.txt'.format(work, sv_type)
-        output = '{}/{}.vcf'.format(work, sv_type)
-        with open(input_list, "w") as out:
-            out.write("\n".join(input_vcfs[sv_type]))
-        cmd = ['java', '-cp', '{}/jasmine_iris.jar:{}/jasmine.jar'.format(jasmine_dir, jasmine_dir), 'Main',
-               'iris_args=samtools_path=samtools,racon_path=racon,minimap_path=minimap2',
-               'file_list={}'.format(input_list), 'out_file={}'.format(output), 'threads={}'.format(threads),
-               'out_dir={}'.format(work), 'max_dist_linear={}'.format(max_dist_linear),
-               'min_overlap={}'.format(min_overlap)]
-        print(' '.join(cmd))
+def mean(x):
+    return round(sum(x) / len(x))
+
+
+def merge(jasmine_vcfs, records):
+    all_records = {}
+    for recs in records.values():
+        for sv_type, rs in recs.items():
+            if sv_type == 'HEADER':
+                continue
+            if sv_type not in all_records:
+                all_records[sv_type] = {}
+            all_records[sv_type].update(rs)
+
+    for sv_type in jasmine_vcfs:
+        vf = VariantFile(jasmine_vcfs[sv_type])
+        info_out = '{}/{}.info.bcf'.format(WORK, sv_type)
+        vf_out = VcfWriter(info_out, vf.header, remove=REMOVE, drop=True, sort=True)
+        for rec in vf:
+            IDS = rec.info.get('IDLIST')
+            recs = [all_records[sv_type][x] for x in IDS]
+            rec.ref = 'N'
+            # rec.qual = mean([x.qual for x in recs])
+            rec.pos = mean([x.pos for x in recs])
+            rec.stop = mean([x.stop for x in recs])
+            rec.info['SVLEN'] = (mean([x.info['SVLEN'][0] for x in recs]),)
+            rec.filter.clear()
+            if any(['PASS' in x.filter for x in recs]):
+                rec.filter.add('PASS')
+            else:
+                for flt in set([y for sl in [x.filter.keys() for x in recs] for y in sl]):
+                    rec.filter.add(flt)
+            for r in recs:
+                r.id = rec.id
+                r.pos = rec.pos
+                r.ref = 'N'
+                r.filter.clear()
+                r.filter.add('PASS')
+            vf_out.write(rec)
+        vf_out.close(index=True)
+        sample_outs = []
+        for pref in records:
+            if sv_type not in records[pref]:
+                continue
+            pref_out = '{}/{}.{}.clean.bcf'.format(WORK, pref, sv_type)
+            header = records[pref]['HEADER']
+            vf_out = VcfWriter(pref_out, header, remove=['INFO'], sort=True)
+            for rec in records[pref][sv_type].values():
+                vf_out.write(rec)
+            vf_out.close(index=True)
+            sample_outs.append(pref_out)
+        output = '{}.merged.vcf.gz'.format(sv_type)
+        cmd = ['bcftools', 'merge', '-m', 'id', '--missing-to-ref', '-Oz', '-o', output, info_out]
+        cmd.extend(sample_outs)
         proc = Popen(cmd, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-        proc.communicate()
-        proc = Popen(['bcftools', 'view', output, '-Oz', '-o', '{}.gz'.format(output)],
-                     stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
-        proc.communicate()
+        proc.wait()
+        print("created " + output)
 
 
-def main(inputs, output, work, split_types, jasmine_jar):
-    if not os.path.exists(work):
-        os.makedirs(work)
-    stripped = strip_vcfs(inputs, work, split_types)
-    jasmine(stripped, output, jasmine_jar, work)
+def main(inputs, output, sv_types, jasmine_dir, jasmine_args):
+    jasmine_cp = get_jasmine_cp(jasmine_dir)
+    if not os.path.exists(WORK):
+        os.makedirs(WORK)
+    vcfs, records = proc_vcfs(inputs, sv_types)
+    jasmine_vcfs = jasmine(vcfs, jasmine_cp, jasmine_args)
+    merge(jasmine_vcfs, records)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('inputs', nargs='+', help='input sample vcfs to be merged')
-    parser.add_argument('--output', default='output', help='output variant filename')
+    parser.add_argument('--output', default='output', help='prefix of output variant filename(s)')
     parser.add_argument('--work', default='work', help='working directory')
     parser.add_argument('--jasmine-dir', default='/bin', help='path to jasmine jar file')
-    parser.add_argument('--split-types', action='store_true', help='split sv types prior to jasmine process')
+    parser.add_argument('--sv-type', action='append', help='process only this SVTYPE')
     args = parser.parse_args()
-    main(args.inputs, args.output, args.work, args.split_types, args.jasmine_dir)
+    jasmine_args = ['threads=2',
+                    'max_dist_linear=0.20',
+                    'max_dist=500',
+                    'min_overlap=0.80']
+    WORK = args.work
+    main(args.inputs, args.output, args.sv_type, args.jasmine_dir, jasmine_args)
+
+# TODO: add option --jasmine-args to pass through arbitrary jasmine args
